b9de4dcf5fb3d124e1e8f22ba7672b79dd10a952
zetashift <rskaraya@gmail.com>
2023-01-12 21:13:41 +0100

    Remove parenthesis

--- core/js/src/main/scala/tf/bug/dirs/MacPlatform.scala
+++ core/js/src/main/scala/tf/bug/dirs/MacPlatform.scala
@@ -4,15 +4,15 @@ import java.nio.file.{Path, Paths}
 
 object MacPlatform extends Mac {
   def home: Option[Path] = Some(Paths.get(os.homedir()))
-  def cache: Option[Path] = home.map(h => Paths.get(h + "/Library/)Caches"))
+  def cache: Option[Path] = home.map(h => Paths.get(h + "/Library/Caches"))
   def config: Option[Path] =
-    home.map(h => Paths.get(h + "/Library/)Application Support"))
+    home.map(h => Paths.get(h + "/Library/Application Support"))
   def data: Option[Path] =
-    home.map(h => Paths.get(h + "/Library/)Application Support"))
+    home.map(h => Paths.get(h + "/Library/Application Support"))
   def dataLocal: Option[Path] =
-    home.map(h => Paths.get(h + "/Library/)Preferences"))
+    home.map(h => Paths.get(h + "/Library/Preferences"))
   def preference: Option[Path] =
-    home.map(h => Paths.get(h + "/Library/)Preferences"))
+    home.map(h => Paths.get(h + "/Library/Preferences"))
   def audio: Option[Path] = home.map(h => Paths.get(h + "/Music"))
   def desktop: Option[Path] = home.map(h => Paths.get(h + "/Desktop"))
   def document: Option[Path] = home.map(h => Paths.get(h + "/Documents"))
